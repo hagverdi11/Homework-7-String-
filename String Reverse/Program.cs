94295e870222705d4af39365067a52af3dbe600f
hagverdi11 <hagverdiam@code.edu.az>
2022-06-22 03:41:47 +0400

    Added homework table

---
+++ String Reverse/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+
+            //Homework7 Reverse words.
+
+
+            Console.WriteLine(reverse("Haqverdi"));
+            
+        }
+        public static string reverse(string s)
+        {
+            string reverse = "";
+            for (int a = s.Length - 1; a >= 0; a--)
+            {
+                reverse = reverse + s[a];
+            }
+            return reverse;
+        }
+    }
+}
